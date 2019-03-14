aa1b79ab78b3270f2428f0d9c12cf6a046aa2231
laizhiren <laizhiren@aspirecn.com>
2019-03-14 17:26:05 +0800

    高保修改

--- src/js/index.js
+++ src/js/index.js
@@ -29,19 +29,19 @@ import '../js/mmapp.js'; */
     /* 普通确认框 */
     var dialog = window.YDUI.dialog;
 
-    dialog.confirm('您的订单已受理', '您已成功提交MM任我装流浪包订购申请，成功受理信息请以中国移动下发短信为准，目前由于订购用户较多，短信可能有延时，请耐心等待。', [
-        {
-            txt: '',
-            color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-            callback: function () {
-                dialog.toast('你点了取消', 'none', 1000);
-            }
-        }
+    // dialog.confirm('您的订单已受理', '您已成功提交MM任我装流浪包订购申请，成功受理信息请以中国移动下发短信为准，目前由于订购用户较多，短信可能有延时，请耐心等待。', [
+    //     {
+    //         txt: '',
+    //         color: false, /* false:黑色  true:绿色 或 使用颜色值 */
+    //         callback: function () {
+    //             dialog.toast('你点了取消', 'none', 1000);
+    //         }
+    //     }
 
-    ]);
-    setTimeout(function(){
-        $('#YDUI_CONFRIM').remove();
-    },5000)
+    // ]);
+    // setTimeout(function(){
+    //     $('#YDUI_CONFRIM').remove();
+    // },5000)
     $('.mflq-btn').click(function () {
         dialog.confirm('抱歉，订购失败', '抱歉，因网络原因<br/>您本次订购出现错误，请重新领取。', [
             {
@@ -78,6 +78,34 @@ import '../js/mmapp.js'; */
 
         }, 500);
     });
+
+    // !function initClick() {
+    //     var tag = false;
+    //     if (tag) {
+    //         $('.dom').click(function () {
+    //             alert(1)
+    //         })
+    //     } else {
+    //         $('.dom').click(function () {
+    //             alert(2)
+    //         })
+    //     }
+    // }()
+
+    // !function initClick() {
+    //     var tag = false;
+    //     $('.dom').click(function () {
+    //         if (tag) {
+    //             alert(1)
+    //         } else {
+    //             alert(2)
+    //         }
+
+    //     })
+
+    // }()
+
+
 }(window);
 
 
