9be53a82d6db0167545c2681f1449289d470927d
laizhiren <laizhiren@aspirecn.com>
2019-04-01 16:33:36 +0800

    init

--- src/js/index.js
+++ src/js/index.js
@@ -29,23 +29,23 @@ import '../js/mmapp.js'; */
     /* 普通确认框 */
     var dialog = window.YDUI.dialog;
 
-    dialog.confirm('温馨提示', '您已成功提交MM任我装<span class="look-me">爱奇艺黄金VIP会员月卡</span>流浪包订购申请，请耐心等待。', [
-        {
-            txt: '取消',
-            color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-            callback: function () {
-                dialog.toast('你点了取消', 'none', 1000);
-            }
-        },
-        {
-            txt: '确定',
-            color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-            callback: function () {
-                dialog.toast('你点了取消', 'none', 1000);
-            }
-        }
+    // dialog.confirm('温馨提示', '您已成功提交MM任我装<span class="look-me">爱奇艺黄金VIP会员月卡</span>流浪包订购申请，请耐心等待。', [
+    //     {
+    //         txt: '取消',
+    //         color: false, /* false:黑色  true:绿色 或 使用颜色值 */
+    //         callback: function () {
+    //             dialog.toast('你点了取消', 'none', 1000);
+    //         }
+    //     },
+    //     {
+    //         txt: '确定',
+    //         color: false, /* false:黑色  true:绿色 或 使用颜色值 */
+    //         callback: function () {
+    //             dialog.toast('你点了取消', 'none', 1000);
+    //         }
+    //     }
 
-    ]);
+    // ]);
 
     $('.mflq-btn').click(function () {
         dialog.confirm('抱歉，订购失败', '抱歉，因网络原因<br/>您本次订购出现错误，请重新领取。', [
