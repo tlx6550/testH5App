88d18298a2e67429ed1f87bd2af784ab14b410e2
laizhiren <laizhiren@aspirecn.com>
2019-04-03 16:02:08 +0800

    done

--- src/js/index.js
+++ src/js/index.js
@@ -13,20 +13,6 @@ import LotteryCard from '../assets/js/card.js';
 import '../js/mmapp.js'; */
 
 !function (window) {
-    !function initTel() {
-        $('.tel-input').bind('input propertychange', function () {
-            var val = $(this).val();
-            // 移动号段
-            var regtel = /^((13[4-9])|(15([0-2]|[7-9]))|(18[2|3|4|7|8])|(178)|(147))[\d]{8}$/;
-            if (regtel.test(val)) {
-                console.log(val);
-                $('.tel-yanzheng').hide();
-            } else {
-                $('.tel-yanzheng').show();
-            }
-        });
-    }();
-
     /* 普通确认框 */
     var dialog = window.YDUI.dialog;
     // 弹窗背景表情变化，title传‘sad’是未中奖表情，传good或者不传是高兴表情
@@ -54,38 +40,27 @@ import '../js/mmapp.js'; */
     // [ ]);
 
     //第一次提示
-    // dialog.fisrtStep('提示:',
-    //     '<span>下载应用后，一定要在本活动页面</span>' +
-    //     '<span class="look-me">【打开】</span>' +
-    //     '<span>应用，才能获得刮卡机会</span>'
-    // );
-    // setTimeout(function () {
-    //     $('#YDUI_CONFRIM').remove();
-    //     dialog.shareOnWeb('',
-    //         '<div class="share-on-web"><span>1.微信不支持下载，请点击右上角按钮</span></div>' +
-    //         '<div class="share-on-web"><span>2.在菜单中点击</span><span class="look-me">【在浏览器中打开】</span>' +
-    //         '</div>',
-    //         []);
-    //     setTimeout(function () {
-    //         $('#YDUI_CONFRIM').remove();
-    //     }, 2000)
-    // }, 2000)
-    $('.mflq-btn').click(function () {
-        dialog.confirm('抱歉，订购失败', '抱歉，因网络原因<br/>您本次订购出现错误，请重新领取。', [
-            {
-                txt: '确定',
-                color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-                callback: function () {
-                    dialog.toast('你点了取消', 'none', 1000);
-                }
-            }
-
-        ]);
-    });
+    dialog.fisrtStep('提示:',
+        '<span>下载应用后，一定要在本活动页面</span>' +
+        '<span class="look-me">【打开】</span>' +
+        '<span>应用，才能获得刮卡机会</span>'
+    );
+    setTimeout(function () {
+        $('#YDUI_CONFRIM').remove();
+        dialog.shareOnWeb('',
+            '<div class="share-on-web"><span>1.微信不支持下载，请点击右上角按钮</span></div>' +
+            '<div class="share-on-web"><span>2.在菜单中点击</span><span class="look-me">【在浏览器中打开】</span>' +
+            '</div>',
+            []);
+        setTimeout(function () {
+            $('#YDUI_CONFRIM').remove();
+        }, 2000)
+    }, 2000)
 
 }(window);
 
 !function (window) {
+    //刮奖结果卡片小猪状态切换、传true表示中奖
     function changeResultPigImg(tag){
         if(tag){
             $('.result-pig').removeClass('result-bad').addClass('result-good')
@@ -93,17 +68,17 @@ import '../js/mmapp.js'; */
             $('.result-pig').removeClass('result-good').addClass('result-bad')
         }
     }
-    // 刮刮乐
+    //中奖文案修改
+    $('.result-text').find('.title').html('刮奖失败，请使用本机<br>手机号码登录MM');
+    $('.result-text').find('.info-detail').html('(打开4G蜂窝网络开关)');
+    // 初始化刮刮乐
     function initGuaGuaLe() {
         var canvas = $('.canvas_lottery')[0];
         var ctx = canvas.getContext('2d');
         var img = new Image();
         var urlImg = $('.guauga-bg-img').attr('src');
-        console.log('src=' + urlImg);
         img.src = urlImg;
-
         img.onload = function () {
-
             var html = $('html')[0].style;
             var baseSize = parseFloat(html.fontSize);
             // console.log(baseSize);
