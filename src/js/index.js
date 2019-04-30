bf6aad4ac120180949b603402f6b3f3f110896ac
laizhiren <laizhiren@aspirecn.com>
2019-04-30 17:17:59 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -12,23 +12,22 @@ import '../js/mmapp.js'; */
 !function (window) {
     /* 普通确认框 */
     var dialog = window.YDUI.dialog;
-    // 弹窗背景表情变化，title传‘sad’是未中奖表情，传good或者不传是高兴表情
-       dialog.confirm('good', '恭喜您，获得<span class="look-me">3GB流量</span>。', [
-       {
-               txt: '取消',
-               color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-               callback: function () {
-                   dialog.toast('你点了确定', 'none', 1000);
-               }
-           },
-           {
-               txt: '确定',
-               color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-               callback: function () {
-                   dialog.toast('你点了确定', 'none', 1000);
-               }
-           }
-       ]);
+    //    dialog.confirm('good', '恭喜您，获得<span class="look-me">3GB流量</span>。', [
+    // //    {
+    // //            txt: '取消',
+    // //            color: false, /* false:黑色  true:绿色 或 使用颜色值 */
+    // //            callback: function () {
+    // //                dialog.toast('你点了确定', 'none', 1000);
+    // //            }
+    // //        },
+    //     //    {
+    //     //        txt: '确定',
+    //     //        color: false, /* false:黑色  true:绿色 或 使用颜色值 */
+    //     //        callback: function () {
+    //     //            dialog.toast('你点了确定', 'none', 1000);
+    //     //        }
+    //     //    }
+    //    ]);
     //其它浏览器分享
     // dialog.shareOnWeb('good', 
     // '<div class="share-on-web"><span>1.微信不支持下载，请点击<br>右上角按钮</span></div>'+
@@ -36,12 +35,7 @@ import '../js/mmapp.js'; */
     // '</div>', 
     // [ ]);
 
-    //第一次提示
-    // dialog.fisrtStep('提示',
-    //     '<span>下载应用后，一定要在本活动页面</span><br>' +
-    //     '<span>【打开】</span>' +
-    //     '<span>应用，才能获得金币。</span>'
-    // );
+
     // setTimeout(function () {
     //     $('#YDUI_CONFRIM').remove();
     //     dialog.shareOnWeb('good', 
