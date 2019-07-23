7faa2914c84ce5d5f7b5917da1ba1835a575588b
laizhiren <laizhiren@aspirecn.com>
2019-07-23 09:04:46 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -7,76 +7,13 @@ import $ from '../assets/js/jquery.min.js';
 import '../assets/js/flexible.js';
 import '../components/dialog/dialog.js';
 import swiper from '../assets/js/swiper.min.js';
+//import BScroll from '@better-scroll/core';
 // app download
 
 /* import '../js/mmdl.js';
 import '../js/mmapp.js'; */
 
-!function (window) {
-    !function initTel() {
-        $('.tel-input').bind('input propertychange', function () {
-            var val = $(this).val();
-            // 移动号段
-            var regtel = /^((13[4-9])|(15([0-2]|[7-9]))|(18[2|3|4|7|8])|(178)|(147))[\d]{8}$/;
-            if (regtel.test(val)) {
-                console.log(val);
-                $('.tel-yanzheng').hide();
-            } else {
-                $('.tel-yanzheng').show();
-            }
-        });
-    }();
-
-    /* 普通确认框 */
-    var dialog = window.YDUI.dialog;
-
-    // dialog.confirm('您的订单已受理', '您已成功提交MM任我装流浪包订购申请，成功受理信息请以中国移动下发短信为准，目前由于订购用户较多，短信可能有延时，请耐心等待。', [
-    //     {
-    //         txt: '确定',
-    //         color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-    //         callback: function () {
-    //             dialog.toast('你点了取消', 'none', 1000);
-    //         }
-    //     }
-    //
-    // ]);
-    $('.mflq-btn').click(function () {
-        dialog.confirm('抱歉，订购失败', '抱歉，因网络原因<br/>您本次订购出现错误，请重新领取。', [
-            {
-                txt: '确定',
-                color: false, /* false:黑色  true:绿色 或 使用颜色值 */
-                callback: function () {
-                    dialog.toast('你点了取消', 'none', 1000);
-                }
-            }
-
-        ]);
-    });
-    // 获取验证码逻辑
-    var $getCode = $('#J_GetCode');
-
-    /* 定义参数 */
-    $getCode.sendCode({
-        disClass: 'btn-disabled',
-        secs: 10,
-        run: false,
-        runStr: '{%s}秒后重新获取',
-        resetStr: '重新获取验证码'
-    });
-
-    $getCode.on('click', function () {
-        /* ajax 成功发送验证码后调用【start】 */
-        YDUI.dialog.loading.open('发送中');
-        setTimeout(function () {
-
-            YDUI.dialog.loading.close();
-
-            $getCode.sendCode('start');
-            YDUI.dialog.toast('已发送', 'success', 500);
-
-        }, 500);
-    });
-}(window);
+!
 !function (window) {
     // tabl逻辑
     var $header = $('.tab-header');
@@ -91,93 +28,30 @@ import '../js/mmapp.js'; */
 //          $header.children('div').eq(index).addClass('active');
 //      }
 //  });
-    var innerBannerSwiper = new Swiper('.inner-swiper', {
-        loop: true,
-        autoplay : 14000,
-        autoplayDisableOnInteraction : false,
-        pagination: '.swiper-pagination',
-       paginationClickable: true,
-//      spaceBetween: 8,
-//      slidesPerView: 1.15,
-//      centeredSlides: true,
-//      onSlideChangeEnd: function(swiper) {
-//          var index = swiper.activeIndex;
-//          var len = $('.inner-swiper .banner-wrap').length;
-//          console.log(len)
-//          if(index == 0) {  // 如果是第一张，就滑动到第二张
-//              innerBannerSwiper.slideTo(1, 250); // 滑动时间250毫秒
-//          }
-//          if(index == len-2){   // 如果是倒数第二张，就回到第二张
-//              setTimeout(function(){
-//                  innerBannerSwiper.slideTo(1,250);   
-//              },4000);
-//          }
-//          if(index== len-1){  // 如果是倒数第一张，就回到倒数第二张
-//              innerBannerSwiper.slideTo(len-2,250);   
-//          }
-//      }
+   
+    var outerBannerSwiper = new Swiper('.outer-swiper', {
+        spaceBetween: 20,
+        onSlideChangeStart: function(swiper){
+            var index = swiper.activeIndex;
+            $header.children('div').removeClass('active');
+            $header.children('div').eq(index).addClass('active');
+        }
+    }); 
+    var wrapper1 = document.querySelector('.btscroll1', {
+        probeType: 3,
     });
+    
     headers.forEach(function (o, i) {
         o.addEventListener('touchend', function () {
-            console.log(i);
             var index = i;
             $header.children('div').removeClass('active');
             $header.children('div').eq(index).addClass('active');
+            outerBannerSwiper.slideTo(index);
 //          mySwiper.slideTo(index);
         });
     });
-    !function initIframH(){
-        var bodyH = document.body.clientHeight;
-        var headerH = $('.tab-header').height();
-        var iframH = Math.floor(bodyH-headerH);
-        $('.flexiframe').css('height',iframH + 'px');
-    }();
-    // var startX,startY,moveEndX,moveEndY,X,Y;
-    // $("body").on("touchstart", function(e) {
-    //     // 判断默认行为是否可以被禁用
-    //     if (e.cancelable) {
-    //         // 判断默认行为是否已经被禁用
-    //         if (!e.defaultPrevented) {
-    //             e.preventDefault();
-    //         }
-    //     }   
-    //     startX = e.originalEvent.changedTouches[0].pageX;
-    //     startY = e.originalEvent.changedTouches[0].pageY;
-    // });
-    // $("body").on("touchend", function(e) {         
-    //     // 判断默认行为是否可以被禁用
-    //     if (e.cancelable) {
-    //         // 判断默认行为是否已经被禁用
-    //         if (!e.defaultPrevented) {
-    //             e.preventDefault();
-    //         }
-    //     }               
-    //     moveEndX = e.originalEvent.changedTouches[0].pageX,
-    //     moveEndY = e.originalEvent.changedTouches[0].pageY,
-    //     X = moveEndX - startX,
-    //     Y = moveEndY - startY;
-    //     //左滑
-    //     if ( X > 0 ) {
-    //         alert('左滑');                
-    //     }
-    //     //右滑
-    //     else if ( X < 0 ) {
-    //         alert('右滑');    
-    //     }
-    //     //下滑
-    //     else if ( Y > 0) {
-    //         alert('下滑');    
-    //     }
-    //     //上滑
-    //     else if ( Y < 0 ) {
-    //         alert('上滑');    
-    //     }
-    //     //单击
-    //     else{
-    //         alert('单击');    
-    //     }
-    // });
 
+   
 
 }(window);
 
