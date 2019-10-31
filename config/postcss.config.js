2d22a90de15968dca06be3fcc7d28cc1a32a95c4
laizhiren <laizhiren@aspirecn.com>
2019-10-31 10:20:35 +0800

    使用插件css直接使用px单位

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -6,6 +6,17 @@ module.exports = {
         },*/
         autoprefixer: {
             browsers: ['last 3 versions','iOS >= 8','Firefox >= 20','Android > 4.2','not ie <= 8']
+        },
+        'postcss-plugin-px2rem':{
+        	rootValue: 100,
+            unitPrecision: 8,
+            propWhiteList: [],
+            propBlackList: [],
+            selectorBlackList: [],
+            ignoreIdentifier: false,
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 2     
         }
 
     }
