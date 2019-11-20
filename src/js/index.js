58d380895a71e1110cc178dc6ea69f245e05638f
laizhiren <laizhiren@aspirecn.com>
2019-11-20 09:28:44 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,24 @@ import '../components/dialog/dialog.js';
 
 /* import '../js/mmdl.js';
 import '../js/mmapp.js'; */
-
-window.onload = function(){
-
+var dialog = window.YDUI.dialog;
+window.onload = function () {
+    dialog.confirm('温馨提示', '您（13911111111）已成功订购<br><div class="look-me">0元5GB定向流量包，</div>获得抽奖机会，赶紧去抽奖吧~', [
+        {
+            txt: '取消',
+            color: false,
+            /* false:黑色  true:绿色 或 使用颜色值 */
+            callback: function () {
+                dialog.toast('你点了取消', 'none', 1000);
+            }
+        },
+        {
+            txt: '确定',
+            color: false,
+            /* false:黑色  true:绿色 或 使用颜色值 */
+            callback: function () {
+                dialog.toast('确定', 'none', 1000);
+            }
+        }
+    ]);
 };
